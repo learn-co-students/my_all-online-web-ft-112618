690c8dc3ee5ca36d74ff022e74409680762c3a6b
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:15:49 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  boolean_array = Array.new
+  while i < collection.length
+    boolean_array << yield(collection[i])
+    i += 1
+  end
 
+  !(boolean_array.include?(false))
+
+  # binding.pry
 end
+
+puts my_all?([1, 2, 3]) { |x| x < 2 }
